13463462adcb85599f6c264b0d1b577b8aa4e4b2
도미닉 <kiljh@me.com>
2020-11-07 08:26:42 +0900

    aspectRatio

--- HelloSwiftUI/HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -10,13 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack(spacing: 30) {
-            Image("SwiftUI").resizable().frame(width: 100, height: 150)
-            
-            Image("SwiftUI").scaledToFit().frame(width: 100, height: 150)
+            Image("SwiftUI").resizable()
+                .aspectRatio(CGSize(width: 1.6, height: 1), contentMode: .fit)
+                .frame(width:150, height:150)
             
             Image("SwiftUI").resizable()
-                .scaledToFill()
-                .frame(width: 100, height: 150)
+                .aspectRatio(0.7, contentMode: .fill)
+                .frame(width:150, height:150)
                 .clipped()
         }
     }
