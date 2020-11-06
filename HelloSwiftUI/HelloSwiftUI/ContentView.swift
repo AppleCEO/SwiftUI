05b7dc60debeb29771ff0f24724ef6191d4ed4a4
도미닉 <kiljh@me.com>
2020-11-07 08:22:44 +0900

    ContentMode

--- HelloSwiftUI/HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -9,10 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            Image("SwiftUI")
-                .resizable(capInsets: .init(top: 0, leading: 50, bottom: 0, trailing: 0), resizingMode: .tile)
-                .frame(width: 150, height: 150)
+        HStack(spacing: 30) {
+            Image("SwiftUI").resizable().frame(width: 100, height: 150)
+            
+            Image("SwiftUI").scaledToFit().frame(width: 100, height: 150)
+            
+            Image("SwiftUI").resizable()
+                .scaledToFill()
+                .frame(width: 100, height: 150)
+                .clipped()
         }
     }
 }
