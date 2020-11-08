52db832bc47da1d2fd1319cb065d3c5cd1f49872
도미닉 <kiljh@me.com>
2020-11-08 19:37:01 +0900

    zstack 을 이용한 구현

--- HelloSwiftUI/HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -9,15 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Circle()
-            .fill(Color.yellow.opacity(0.8))
-            .frame(width: 250, height: 250)
-            .overlay(Text("Joystick").font(.largeTitle))
-            .overlay(Image(systemName: "arrow.up").font(.title).padding(), alignment: .top)
-            .overlay(Image(systemName: "arrow.left").font(.title).padding(), alignment: .leading)
-            .overlay(Image(systemName: "arrow.up.right.circle.fill").font(.title), alignment: .topTrailing)
-            .background(Image(systemName: "arrow.down").font(.title).padding(), alignment: .bottom)
-            .background(Image(systemName: "arrow.right").font(.title).padding(), alignment: .trailing)
+        ZStack {
+            VStack {
+                Spacer()
+                Image(systemName: "arrow.down").font(.title).padding()
+            }
+            HStack {
+                Spacer()
+                Image(systemName: "arrow.right").font(.title).padding()
+            }
+            Circle()
+                .fill(Color.yellow.opacity(0.8))
+                .frame(width: 250, height: 250)
+            
+            Text("Joystick").font(.largeTitle)
+            
+            ZStack(alignment: .topTrailing) {
+                Color.clear
+                Image(systemName: "arrow.up.right.circle.fill").font(.title)
+            }
+            VStack {
+                Image(systemName: "arrow.up").font(.title).padding()
+                Spacer()
+            }
+            HStack {
+                Image(systemName: "arrow.left").font(.title).padding()
+                Spacer()
+            }
+        }.frame(width: 250, height: 250)
     }
 }
 
