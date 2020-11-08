0183e695679b63c929b1b467bd78919d5b6db5e3
도미닉 <kiljh@me.com>
2020-11-08 19:21:27 +0900

    overlay

--- HelloSwiftUI/HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -9,34 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("도형 만들기").font(.largeTitle).fontWeight(.heavy)
-            HStack {
-                Text("둥근 모양").font(.title)
-                Spacer()
-            }
-            ZStack {
-                Rectangle().frame(height: 10)
-                HStack {
-                    Circle().fill(Color.yellow)
-                    Ellipse().fill(Color.green)
-                    Capsule().fill(Color.orange)
-                    RoundedRectangle(cornerRadius: 30).fill(Color.gray)
-                }
-            }
-            HStack {
-                Text("각진 모양").font(.title)
-                Spacer()
-            }
-            ZStack {
-                Rectangle().frame(height: 10)
-                HStack {
-                    Color.red
-                    Rectangle().fill(Color.blue)
-                    RoundedRectangle(cornerRadius: 0).fill(Color.purple)
-                }
-            }
-        }.padding()
+        Rectangle().fill(Color.green).frame(width: 150, height: 150)
+            .overlay(
+                Rectangle().fill(Color.yellow)
+            )
     }
 }
 
