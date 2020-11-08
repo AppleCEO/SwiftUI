97624c1a576a6f377f628b100c718dd359bfde8f
도미닉 <kiljh@me.com>
2020-11-08 18:59:37 +0900

    Spacer 를 이용한 배치

--- HelloSwiftUI/HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -9,10 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color.clear
-            Text("Spacer").font(.title).background(Color.yellow)
-        }.background(Color.blue)
+        VStack {
+            Text("제목").font(.largeTitle)
+            Text("부제목").foregroundColor(Color.gray)
+            Spacer()
+            Text("본문 내용")
+            Spacer()
+            Spacer()
+            Text("각주").font(.body)
+        }
+        .font(.title)
+        .frame(width: 200, height: 350)
+        .padding()
+        .border(Color.blue, width: 2)
     }
 }
 
