9bda1dbb32d6ce69ffffa0e907168a18f4c9973f
도미닉 <kiljh@me.com>
2020-11-12 22:12:20 +0900

    GroupedListStyle

--- HelloSwiftUI/HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -24,7 +24,7 @@ struct ContentView: View {
                     }
                 }
             }
-        }
+        }.listStyle(GroupedListStyle())
     }
     
     let fruits = ["사과", "배", "포도", "바나나"]
