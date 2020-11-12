07f9ad2b1c2dc1c746326301347c0d1299915b19
도미닉 <kiljh@me.com>
2020-11-12 22:08:20 +0900

    섹션

--- HelloSwiftUI/HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -8,21 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    let fruits = ["사과", "배", "포도", "바나나"]
-    let drinks = ["물", "우유", "탄산수"]
+    
     var body: some View {
-        List {
-            Text("Fruits").font(.largeTitle)
-            ForEach(fruits, id: \.self) {
-                Text($0)
-            }
-            
-            Text("Drinks").font(.largeTitle)
-            ForEach(drinks, id: \.self) {
-                Text($0)
+        let titles = ["Fruits", "Drinks"]
+        let data = [fruits, drinks]
+        
+        return List {
+            ForEach(data.indices) { index in
+                Section(
+                    header: Text(titles[index]).font(.title),
+                    footer: HStack { Spacer(); Text("\(data[index].count)건")}
+                ) {
+                    ForEach(data[index], id: \.self) {
+                        Text($0)
+                    }
+                }
             }
         }
     }
+    
+    let fruits = ["사과", "배", "포도", "바나나"]
+    let drinks = ["물", "우유", "탄산수"]
 }
 
 struct ContentView_Previews: PreviewProvider {
