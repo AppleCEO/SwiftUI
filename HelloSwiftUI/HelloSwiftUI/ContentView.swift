ca618ea75e5113ee76a1c623c8461840e7bb3205
도미닉 <kiljh@me.com>
2020-11-12 10:53:27 +0900

    리스트 안에 정적 콘텐츠

--- HelloSwiftUI/HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -10,16 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         List {
-            Text("1")
-            Text("2")
-            Text("3")
-            Text("4")
-            Text("5")
-            Text("6")
-            Text("7")
-            Text("8")
-            Text("9")
-            Text("10")
+            Text("List").font(.largeTitle)
+            Image("SwiftUI")
+            Circle().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+            Color(.red).frame(width: 100, height: 100)
         }
     }
 }
