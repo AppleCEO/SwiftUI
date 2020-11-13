f2d7482c269128bc91900e9e5bc21eacdbf46e77
도미닉 <kiljh@me.com>
2020-11-13 14:16:35 +0900

    fixedSize 미적용

--- HelloSwiftUI/HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -10,18 +10,12 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        HStack {
-            Rectangle().fill(Color.red).frame(minWidth: 100)
-            Rectangle().fill(Color.orange).frame(maxWidth: 15)
-            
-            Rectangle().fill(Color.yellow).frame(height: 150)
-            Rectangle().fill(Color.green).frame(maxHeight: .infinity)
-            
-            Rectangle().fill(Color.blue)
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-            Rectangle().fill(Color.purple)
-            
-        }.frame(width: 300, height: 150)
+        VStack {
+            Text("Frame Modifier").font(.title).bold().frame(width: 80, height: 30)
+            Rectangle()
+            Color.red
+            Image("SwiftUI").resizable()
+        }
     }
     
 }
