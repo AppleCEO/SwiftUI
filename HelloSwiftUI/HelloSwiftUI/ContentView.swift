39bb7f6ed1f266041273bb586386335f8f12fd4f
도미닉 <kiljh@me.com>
2020-11-15 22:51:31 +0900

    State 변경

--- HelloSwiftUI/HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -8,40 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    @State private var framework: String = "UIKit"
     var body: some View {
-        VStack(spacing: 20) {
-            HStack {
-                Color.red
-                Color.green
-                Color.blue
-            }.frame(height: 40)
-            
-            HStack {
-                Color.red.layoutPriority(1)
-                Color.green
-                Color.blue.layoutPriority(1)
-            }.frame(height: 40)
-            
-            HStack {
-                Color.red.layoutPriority(1)
-                Color.green.frame(minWidth: 30)
-                Color.blue.frame(maxWidth: 50).layoutPriority(1)
-            }.frame(height: 40)
-            
-            HStack {
-                Color.red.frame(width: 50)
-                Color.green.layoutPriority(1)
-                Color.blue.frame(maxWidth: 50).layoutPriority(1)
-            }.frame(height: 40)
+        Button(framework) {
+            self.framework = "SwiftUI"
         }
     }
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .previewLayout(.fixed(width: 812, height: 375))
     }
 }
