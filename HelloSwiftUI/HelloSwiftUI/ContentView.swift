b41f67858fae7acc2aedb395939303590300531b
도미닉 <kiljh@me.com>
2020-11-15 20:38:49 +0900

    previewLayout fixed

--- HelloSwiftUI/HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -42,6 +42,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .previewLayout(.sizeThatFits)
+            .previewLayout(.fixed(width: 350, height: 150))
     }
 }
