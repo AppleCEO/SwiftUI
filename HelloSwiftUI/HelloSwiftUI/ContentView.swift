b7874e76e0ecc4e93f0d6fbdbf3f136bdb2fe2df
도미닉 <kiljh@me.com>
2020-11-15 22:55:52 +0900

    @State와 @Binding

--- HelloSwiftUI/HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -8,10 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var framework: String = "UIKit"
+    @State private var isFavorite = true
+    @State private var count = 0
+    
     var body: some View {
-        Button(framework) {
-            self.framework = "SwiftUI"
+        VStack(spacing: 30) {
+            Toggle(isOn: $isFavorite) {
+                Text("isFavorite : \(isFavorite.description)")
+            }
+            Stepper("Count: \(count)", value: $count)
         }
     }
 }
