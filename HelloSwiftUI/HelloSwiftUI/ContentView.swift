56893791e8ae7615a344f33d7a5afbe8ae27f1c8
도미닉 <kiljh@me.com>
2020-11-18 21:58:28 +0900

    Sheet

--- HelloSwiftUI/HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -8,22 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingActionSheet = false
+    @State private var showingSheet = false
     
     var body: some View {
-        Button(action: { self.showingActionSheet = true }) {
-            Text("ActionSheet")
+        Button(action: { self.showingSheet.toggle() }) {
+            Text("Present")
+                .font(.title).foregroundColor(.blue)
         }
-        .actionSheet(isPresented: $showingActionSheet) {
-            ActionSheet(title: Text("제목"),
-                        message: Text("내용"),
-                        buttons: [
-                            .default(Text("버튼1")),
-                            .default(Text("버튼2")),
-                            .destructive(Text("버튼3")),
-                            .cancel(Text("취소")),
-                        ]
-            )
+        .sheet(isPresented: $showingSheet, onDismiss: { print("Dismissed")},
+               content: { PresentedView() })
+    }
+}
+
+struct PresentedView: View {
+    @Environment(\.presentationMode) var presentationMode
+    var body: some View {
+        Button(action: {
+            if self.presentationMode.wrappedValue.isPresented {
+                self.presentationMode.wrappedValue.dismiss()
+            }
+        }) {
+            Text("Tap to Dismiss")
+                .font(.title).foregroundColor(.red)
         }
     }
 }
