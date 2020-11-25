2c13376a3ffc43adcc2d75963ec96c8e62885b6d
도미닉 <kiljh@me.com>
2020-11-25 22:38:02 +0900

    sheet

--- HelloSwiftUI/HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -8,16 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    @ObservedObject var user: User = User()
+    @State private var showingSheet = false
     
     var body: some View {
-        VStack(spacing: 30) {
-            Text(user.name)
-            
-            Button(action: { self.user.score += 1 }) {
-                Text(user.score.description)
-            }
+        Button(action: { self.showingSheet.toggle() }) {
+            Text("Present")
+                .font(.title).foregroundColor(.blue)
         }
+        .sheet(isPresented: $showingSheet, onDismiss: { print("Dismissed")},
+               content: { PresentedView() })
     }
 }
 
@@ -27,7 +26,16 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-class User: ObservableObject {
-    let name = "User Name"
-    @Published var score = 0
+struct PresentedView: View {
+    @Environment(\.presentationMode) var presentationMode
+    var body: some View {
+        Button(action: {
+            if self.presentationMode.wrappedValue.isPresented {
+                self.presentationMode.wrappedValue.dismiss()
+            }
+        }) {
+            Text("Tap to Dismiss")
+                .font(.title).foregroundColor(.red)
+        }
+    }
 }
