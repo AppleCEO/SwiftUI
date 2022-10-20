3ac4ab1d1b876baf9b48581bd52c6a49519ec253
joonhokil <kiljh@me.com>
2022-10-20 09:45:58 +0900

    스크롤 위치에 따라 뷰 색상 변경

--- HelloSwiftUI/HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -10,50 +10,21 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                Text("Vertical ScrollView").font(.largeTitle)
-                ScrollView {
-                    ForEach(0..<10) { i in
-                        Circle()
-                            .fill(Color(white: 0.2 + (0.8 * Double(i) / 10)))
-                            .frame(width: 80, height: 80)
-                    }
-                    .frame(width: geometry.size.width / 2)
+        ScrollView {
+            ForEach(0..<10) { _ in
+                GeometryReader {
+                    Rectangle()
+                        .fill(self.color(from: $0))
                 }
-                .frame(height: geometry.size.height / 2)
-                .padding(.bottom)
-                
-                Text("Horizontal ScrollView").font(.largeTitle)
-                ScrollView(.horizontal) {
-                    HStack {
-                        ForEach(0..<10) { i in
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(.yellow)
-                                .frame(width: 100, height: 100)
-                                .scaleEffect(1 - (0.7 * CGFloat(i) / 10))
-                        }
-                    }
-                    .frame(height: geometry.size.height / 5)
-                    .padding(.horizontal)
-                }
+                .frame(width: 150, height: 150)
             }
         }
-        
     }
-}
-
-struct PresentedView: View {
-    @Environment(\.presentationMode) var presentationMode
-    var body: some View {
-        Button(action: {
-            if self.presentationMode.wrappedValue.isPresented {
-                self.presentationMode.wrappedValue.dismiss()
-            }
-        }) {
-            Text("Tap to Dismiss")
-                .font(.title).foregroundColor(.red)
-        }
+    
+    func color(from proxy: GeometryProxy) -> Color {
+        let yOffset = proxy.frame(in: .global).minY - 44
+        let color = min(1, 0.2 + Double(yOffset / 1000))
+        return Color(hue: color, saturation: color, brightness: color)
     }
 }
 
