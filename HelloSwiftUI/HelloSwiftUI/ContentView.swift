edb8ff90858ac16609ee6ce08b618b9780673f5e
joonhokil <kiljh@me.com>
2022-10-28 13:54:35 +0900

    텝뷰

--- HelloSwiftUI/HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -8,23 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    enum TabItems { case A, B }
+    @State private var selected = TabItems.A
     
     var body: some View {
-        ScrollView {
-            ForEach(0..<10) { _ in
-                GeometryReader {
-                    Rectangle()
-                        .fill(self.color(from: $0))
-                }
-                .frame(width: 150, height: 150)
-            }
+        TabView(selection: $selected) {
+            Text("A 뷰")
+                .tag(TabItems.A)
+                .tabItem { Image(systemName: "a.circle").imageScale(.large)}
+            
+            Text("B 뷰")
+                .tag(TabItems.B)
+                .tabItem { Image(systemName: "b.circle").imageScale(.large)}
         }
-    }
-    
-    func color(from proxy: GeometryProxy) -> Color {
-        let yOffset = proxy.frame(in: .global).minY - 44
-        let color = min(1, 0.2 + Double(yOffset / 1000))
-        return Color(hue: color, saturation: color, brightness: color)
     }
 }
 
