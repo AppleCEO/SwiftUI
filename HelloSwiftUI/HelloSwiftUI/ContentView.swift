f8eb0ca78208f0f2418e2cff6f7f6b3b2b697bc1
joonhokil <kiljh@me.com>
2022-10-28 14:14:15 +0900

    네트워크 통신

--- HelloSwiftUI/HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -8,18 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
-    enum TabItems { case A, B }
-    @State private var selected = TabItems.A
+    @State private var results = [Result]()
     
     var body: some View {
-        TabView(selection: $selected) {
-            Text("A 뷰")
-                .tag(TabItems.A)
-                .tabItem { Image(systemName: "a.circle").imageScale(.large)}
-            
-            Text("B 뷰")
-                .tag(TabItems.B)
-                .tabItem { Image(systemName: "b.circle").imageScale(.large)}
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
+        }
+        .task {
+            await loadData()
+        }
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
+        } catch {
+            print("Invalid data")
         }
     }
 }
@@ -28,4 +44,14 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
 }
